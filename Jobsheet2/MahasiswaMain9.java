7766d35f652b61b25bd937e90f7786ac5a46f342
Faisal6-web <rizkyfaisal886@gmail.com>
2026-03-02 17:30:55 +0700

    modifikasi percobaan3

--- Jobsheet2/MahasiswaMain9.java
+++ Jobsheet2/MahasiswaMain9.java
@@ -14,5 +14,9 @@ public class MahasiswaMain9 {
         Mahasiswa9 mhs2 = new Mahasiswa9 ("Annisa Nabila", "2141720160", 3.25, "TI 2L");
         mhs2.updateIpk(3.30);
         mhs2.tampilkanInformasi();
+
+        Mahasiswa9 mhs3 = new Mahasiswa9 ( "Faisal Rizky", "254107020224",3.20,  "TI 1H");
+        mhs3.updateIpk(3.90);
+        mhs3.tampilkanInformasi();
     }
 }
