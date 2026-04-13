da59a4d80fc8cb925e95b39424ba36350c609d2c
Faisal6-web <rizkyfaisal886@gmail.com>
2026-04-14 02:01:48 +0700

    Praktikum3

--- Jobsheet6/MahasiswaDemo9.java
+++ Jobsheet6/MahasiswaDemo9.java
@@ -29,5 +29,9 @@ public class MahasiswaDemo9 {
         System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC): ");
         list.bubbleSort();
         list.tampil();
+
+        System.out.println("Data yang sudah terurut menggunakan SELECTION SORT (ASC)");
+        list.SelectionSort();
+        list.tampil();
     }
 }
