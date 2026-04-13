32577b5456f628acf4a212d86d4518f31e097893
Faisal6-web <rizkyfaisal886@gmail.com>
2026-04-13 20:26:17 +0700

    InsertionSort

--- Jobsheet6/SortingMain9.java
+++ Jobsheet6/SortingMain9.java
@@ -3,6 +3,7 @@ public class SortingMain9 {
     public static void main(String[] args) {
     int a[] = {20, 10, 2, 7, 12};
     int b[] = {30, 20, 2, 8, 14};
+    int c[] = {40, 10, 4, 9, 3};
 
     Sorting9 dataurut1 = new Sorting9(a, a.length);
     
@@ -20,5 +21,14 @@ public class SortingMain9 {
     dataurut2.SelectionSort();
     System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
     dataurut2.tampil();
+
+
+    Sorting9 dataurut3 = new Sorting9(c, c.length);
+
+    System.out.println("Data awal 3");
+    dataurut3.tampil();
+    dataurut3.InsertionSort();
+    System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+    dataurut3.tampil();
     }
 }
