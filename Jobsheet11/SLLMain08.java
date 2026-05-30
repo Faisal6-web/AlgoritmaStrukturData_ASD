82fe2592a915334ac14c7affca0e176c15f8cdb8
Faisal6-web <rizkyfaisal886@gmail.com>
2026-05-30 22:41:08 +0700

    ModifikasiPercobaan1Js11

--- Jobsheet11/SLLMain08.java
+++ Jobsheet11/SLLMain08.java
@@ -1,19 +1,26 @@
+import java.util.Scanner;
+
 public class SLLMain08 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         SingleLinkedList08 sll = new SingleLinkedList08();
 
-        Mahasiswa8 mhs1 = new Mahasiswa8("24212200", "Alvaro", "1A", 4.0);
-        Mahasiswa8 mhs2 = new Mahasiswa8("23212201", "Bimon", "2B", 3.8);
-        Mahasiswa8 mhs3 = new Mahasiswa8("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa8 mhs4 = new Mahasiswa8("21212203", "Dirga", "4D", 3.6);
+        System.out.println("=== Tambah Data Mahasiswa ===");
+        System.out.print("Masukkan NIM     : ");
+        String nim = sc.nextLine();
+        System.out.print("Masukkan Nama    : ");
+        String nama = sc.nextLine();
+        System.out.print("Masukkan Kelas    : ");
+        String kelas = sc.nextLine();
+        System.out.println("Masukkan IPK    : ");
+        double ipk = sc.nextDouble();
 
+        Mahasiswa8 mhs = new Mahasiswa8(nim, nama, kelas, ipk);
+
+        sll.addFirst(mhs);
         sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
-        sll.print();
+
+        sc.close();
     }
 }
