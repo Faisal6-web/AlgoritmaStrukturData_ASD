7ee8d3a7bdf2f6e6ec651a5c8a9d951ddcd31544
Faisal6-web <rizkyfaisal886@gmail.com>
2026-06-03 01:33:52 +0700

    Percobaan1Js12

---
+++ Jobsheet12/Node_8.java
@@ -0,0 +1,11 @@
+public class Node_8 {
+    Mahasiswa_8 data;
+    Node_8 prev;
+    Node_8 next;
+
+    public Node_8(Mahasiswa_8 data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
